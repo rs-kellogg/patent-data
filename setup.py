2beaa5f31eec3bf8b56a1ba759da249c8c1df034
Will Thompson <will.k.t@gmail.com>
2019-11-20 09:40:13 -0600

    continuing project setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ocr review',
+    version='0.1.1',
+    author='Will Thompson',
+    author_email='wkt@northwestern.edu',
+    maintainer='Will Thompson',
+    maintainer_email='wkt@northwestern.edu',
+    description='NLP Library for OMOP Abstractor',
+    url='https://github.com/textractortechnologies/omop-abstractor-nlp',
+    packages=find_packages(include=['review', 'review.*']),
+    # package_data={'review': ['data/*.j2']},
+    include_package_data=True,
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+        'pyaml'
+      ],
+    extras_require={
+        'interactive': ['jupyter', 'jupyter-server', 'jupyterlab']
+    },
+    # entry_points={
+    #     'console_scripts': ['load_moodys_tables=abstractor.<module>:main']
+    # },
+    setup_requires=['pytest-runner', 'flake8', 'python-dotenv', 'black'],
+    tests_require=['pytest', 'pytest-console-scripts'],
+)
+
